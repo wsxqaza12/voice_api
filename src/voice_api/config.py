13a71b7425de56b9c5bf908a2209ff6e550276da
chichi <wsxqaza12@gmail.com>
2024-09-07 01:12:34 +0800

    fix: config path

--- src/voice_api/config.py
+++ src/voice_api/config.py
@@ -1,14 +1,15 @@
+FLODER_PATH = "/home/ubuntu/"
 MODEL_PATHS = {
     "woman1": {
-        "refer_audio": "/home/ubuntu/voice/woman1/example.wav",
-        "gpt_weights": "/home/ubuntu/GPT-SoVITS/GPT_weights_v2/woman_voice1-e25.ckpt",
-        "sovits_weights": "/home/ubuntu/GPT-SoVITS/SoVITS_weights_v2/woman_voice1_e4_s128.pth",
+        "refer_audio": FLODER_PATH + "voice/woman1/example.wav",
+        "gpt_weights": FLODER_PATH + "GPT-SoVITS/GPT_weights_v2/woman_voice1-e25.ckpt",
+        "sovits_weights": FLODER_PATH + "GPT-SoVITS/SoVITS_weights_v2/woman_voice1_e4_s128.pth",
         "prompt_text": "而这呢也是选战开跑之后，两位候选人首度的正面交锋，我们看一下呢这场辩论会可以说是呢是创下了好几个历史上的纪录喔。"
     },
     "woman2": {
-        "refer_audio": "/home/ubuntu/voice/woman2/example.wav",
-        "gpt_weights": "/home/ubuntu/GPT-SoVITS/GPT_weights_v2/woman_voice2-e25.ckpt",
-        "sovits_weights": "/home/ubuntu/GPT-SoVITS/SoVITS_weights_v2/woman_voice2_e8_s152.pth",
+        "refer_audio": FLODER_PATH + "voice/woman2/example.wav",
+        "gpt_weights": FLODER_PATH + "GPT-SoVITS/GPT_weights_v2/woman_voice2-e25.ckpt",
+        "sovits_weights": FLODER_PATH + "GPT-SoVITS/SoVITS_weights_v2/woman_voice2_e8_s152.pth",
         "prompt_text": "比较容易会受到橘霉素的污染，而橘霉素本身是有渗毒性的，也被世界卫生组织列为是第三级致癌物。"
     },
     # "woman3": {
@@ -18,15 +19,15 @@ MODEL_PATHS = {
     #     "prompt_text": "一个国家如何跟中国，俄国，还有美国同时称兄道弟，这就是问题所在，不过我们先看到美国跟越南之间的关系"
     # },
     "man1": {
-        "refer_audio": "/home/ubuntu/voice/man2/example.wav",
-        "gpt_weights": "/home/ubuntu/GPT-SoVITS/GPT_weights_v2/man1-e25.ckpt",
-        "sovits_weights": "/home/ubuntu/GPT-SoVITS/SoVITS_weights_v2/man1_e24_s384.pth",
+        "refer_audio": FLODER_PATH + "voice/man2/example.wav",
+        "gpt_weights": FLODER_PATH + "GPT-SoVITS/GPT_weights_v2/man1-e25.ckpt",
+        "sovits_weights": FLODER_PATH + "GPT-SoVITS/SoVITS_weights_v2/man1_e24_s384.pth",
         "prompt_text": "一个国家如何跟中国，俄国，还有美国同时称兄道弟，这就是问题所在，不过我们先看到美国跟越南之间的关系"
     },
     "man2": {
-        "refer_audio": "/home/ubuntu/voice/man1/example.wav",
-        "gpt_weights": "/home/ubuntu/GPT-SoVITS/GPT_weights_v2/man2-e25.ckpt",
-        "sovits_weights": "/home/ubuntu/GPT-SoVITS/SoVITS_weights_v2/man2_e24_s336.pth",
+        "refer_audio": FLODER_PATH + "voice/man1/example.wav",
+        "gpt_weights": FLODER_PATH + "GPT-SoVITS/GPT_weights_v2/man2-e25.ckpt",
+        "sovits_weights": FLODER_PATH + "GPT-SoVITS/SoVITS_weights_v2/man2_e24_s336.pth",
         "prompt_text": "而国人最期待赖政府改革的第一名是什么你知道吗?第一名就是诈骗防治，而至于包括了低薪高通膨的通膨问题。"
     },
     # "man3": {
