628b94aae7b4cda2a2b28d2b4c450de2e19e4799
chichi <wsxqaza12@gmail.com>
2025-05-23 02:19:32 +0800

    version: 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="voice_api",
-    version="1.1.0",
+    version="2.0.0",
     author="Chichieh Huang",
     author_email="cch.chichieh@gmail.com",
     description="A Python wrapper for the Voice API",
